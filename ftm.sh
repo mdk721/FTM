2af948f47f4f65bd09eeb842e27a6b90a0b3b26d
Maydayx2 <45719327+Maydayx2@users.noreply.github.com>
2022-06-04 08:55:10 -0500

    Update ftm.sh

--- ftm.sh
+++ ftm.sh
@@ -1,4 +1,4 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
-tar -zxvf xmrig-6.12.2-linux-x64.tar.gz
-cd xmrig-6.12.2
-./xmrig -o rx.unmineable.com:3333 -a rx -k -t 3 -u FTM:0x7ed09285aee02b826ea9fb1358a9a4108bd6230e.Azure-$RANDOM
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar -zxvf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u FTM:0x7ed09285aee02b826ea9fb1358a9a4108bd6230e.Azure-$RANDOM -p x 
