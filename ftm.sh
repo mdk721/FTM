f1381e36f1b1a297fc2cdf1f5f3879f0e1869b88
Maydayx2 <45719327+Maydayx2@users.noreply.github.com>
2022-06-04 10:01:38 -0500

    Update ftm.sh

--- ftm.sh
+++ ftm.sh
@@ -1,2 +1,1 @@
-sudo apt update
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz && tar xvf xmrig-6.17.0-linux-x64.tar.gz && cd xmrig-6.17.0 && ./xmrig -o rx.unmineable.com:3333 -a rx -k -u FTM:0x7ed09285aee02b826ea9fb1358a9a4108bd6230e.$(echo $(shuf -i 1-9999 -n 1)-PROXY) -p x -t 2
