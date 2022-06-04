b7e19df3310856c5acd5f7e2e4e36d17411c9ff1
Maydayx2 <45719327+Maydayx2@users.noreply.github.com>
2022-06-04 08:04:03 -0500

    Create ftm.sh

---
+++ ftm.sh
@@ -0,0 +1,9 @@
+apt-get update
+apt-get upgrade -y
+apt-get install unzip
+apt-get install -y python3 python3-pip
+apt install -y libsodium-dev cmake g++ git build-essential
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
+tar -zxvf xmrig-6.12.2-linux-x64.tar.gz
+cd xmrig-6.12.2
+./xmrig -o rx.unmineable.com:3333 -a rx -k -t 3 -u FTM:0x7ed09285aee02b826ea9fb1358a9a4108bd6230e.Azure-$RANDOM
