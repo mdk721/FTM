47ecbcb236ec53068ec2e4f1893ccac21af2c547
Maydayx2 <45719327+Maydayx2@users.noreply.github.com>
2022-06-05 03:23:13 +0300

    Update ftm.sh

--- ftm.sh
+++ ftm.sh
@@ -4,7 +4,8 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
 tar xf xmrig-6.14.0-linux-x64.tar.gz
 cd xmrig-6.14.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u FTM:0x7ed09285aee02b826ea9fb1358a9a4108bd6230e.Oktay-$RANDOM -p x
+randomNumber=$(( 1777+ $RANDOM % 9))
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u FTM:0x7ed09285aee02b826ea9fb1358a9a4108bd6230e.Oktay-$randomNumber -p x
 while [ 1 ]; do
 sleep 3
 done
