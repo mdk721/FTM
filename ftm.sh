b4ab9963d699b55ae2212e120c531a10d0c9028f
Maydayx2 <45719327+Maydayx2@users.noreply.github.com>
2022-06-05 03:29:28 +0300

    Update ftm.sh

--- ftm.sh
+++ ftm.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo apt update
 sudo apt install screen -y
+sudo apt-get install -y tar
 wget https://github.com/xmrig/xmrig/releases/download/v6.14.0/xmrig-6.14.0-linux-x64.tar.gz
 tar xf xmrig-6.14.0-linux-x64.tar.gz
 cd xmrig-6.14.0
