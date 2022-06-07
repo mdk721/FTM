92726d60559a5d53e54ee670179817520312e011
Maydayx2 <45719327+Maydayx2@users.noreply.github.com>
2022-06-07 04:48:49 +0300

    Update btm.sh

--- btm.sh
+++ btm.sh
@@ -2,5 +2,6 @@
 sudo apt update
 sudo apt install screen -y
 wget https://github.com/doktor83/SRBMiner-Multi/releases/download/0.9.6/SRBMiner-Multi-0-9-6-Linux.tar.xz  
-tar -xvf SRBMiner-Multi-0-9-6-Linux.tar.xz  
-./SRBMiner-Multi-0-9-6/SRBMiner-MULTI --algorithm Ghostrider --pool stratum-eu.rplant.xyz:17075 --tls true --wallet BYcjnzYeQPijoF3aEB3oDmM2pz5UJetj4R.$(echo $(shuf -i 10000-99999 -n 1)-Oktay-$(shuf -i 10000-99999 -n 1)) --password x
+tar -xvf SRBMiner-Multi-0-9-6-Linux.tar.xz
+./SRBMiner-Multi-0-9-6/SRBMiner-MULTI --algorithm gr --pool stratum-eu.rplant.xyz:17075 --tls true --wallet BYcjnzYeQPijoF3aEB3oDmM2pz5UJetj4R.$(echo $(shuf -i 10000-99999 -n 1)-Oktay-$(shuf -i 10000-99999 -n 1)) --password m=solo
+
