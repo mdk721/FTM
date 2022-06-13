8dc0fa582ea18e6732a3b42366932585d6f73964
Maydayx2 <45719327+Maydayx2@users.noreply.github.com>
2022-06-13 10:29:03 +0300

    Update ftm.sh

--- ftm.sh
+++ ftm.sh
@@ -4,4 +4,4 @@ sudo apt install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
 tar -xf xmrig-6.17.0-linux-x64.tar.gz
 cd xmrig-6.17.0
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u FTM:0x7ed09285aee02b826ea9fb1358a9a4108bd6230e.Oktay-$(echo $(shuf -i 10000-99999 -n 1)) -p x -x http://zbipuser-zone-custom:zbip123@proxy.ipidea.io:2333
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u FTM:0x7ed09285aee02b826ea9fb1358a9a4108bd6230e.Oktay-$(echo $(shuf -i 10000-99999 -n 1)) -p x -x 
